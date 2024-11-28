1ba317bd0bbf13c2508dfc40935fdf1f3504a05c
Frederik Höft <43813142+frederik-hoeft@users.noreply.github.com>
2024-11-28 17:33:25 +0100

    clean up throw helper + add support for generating warnings during procedure binding compilation

---
+++ Wkg.EntityFrameworkCore/Wkg.EntityFrameworkCore/ProcedureMapping/Builder/ThrowHelpers/Internals/ArgumentExceptionProxy.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
+
+namespace Wkg.EntityFrameworkCore.ProcedureMapping.Builder.ThrowHelpers.Internals;
+
+internal readonly struct ArgumentExceptionProxy<TArgumentException> where TArgumentException : ArgumentException, new()
+{
+    private readonly ArgumentException _proxiedException;
+
+    public ArgumentExceptionProxy(string message, string? parameterName)
+    {
+        TArgumentException proxiedException = new();
+        ExceptionHelper.SetExceptionMessage(proxiedException, message);
+        if (parameterName is not null)
+        {
+            ExceptionHelper.SetArgumentName(proxiedException, parameterName);
+        }
+        _proxiedException = proxiedException;
+    }
+
+    [DoesNotReturn]
+    [StackTraceHidden]
+    public void Throw() => throw _proxiedException;
+}
---
+++ Wkg.EntityFrameworkCore/Wkg.EntityFrameworkCore/ProcedureMapping/Builder/ThrowHelpers/Internals/ExceptionHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Wkg.EntityFrameworkCore.ProcedureMapping.Builder.ThrowHelpers.Internals;
+
+internal static class ExceptionHelper
+{
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_message")]
+    private static extern ref string GetSetExceptionMessage(Exception exception);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_paramName")]
+    private static extern ref string? GetSetArgumentName(ArgumentException exception);
+
+    public static void SetExceptionMessage(Exception target, string message) => GetSetExceptionMessage(target) = message;
+
+    public static void SetArgumentName(ArgumentException target, string? parameterName) => GetSetArgumentName(target) = parameterName;
+}
